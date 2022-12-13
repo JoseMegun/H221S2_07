cc099d14f0d8ae9efb2a88afb8d9342b279e08cb
José Cama La Madrid <111785479+JoseMegun@users.noreply.github.com>
2022-12-13 11:08:40 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,63 @@
+alert('¡BIENVENIDOS A LA LANDING PAGE OFICIAL DE LA I.E GERARDO S. MEJÍA SACO! Descubra lo más importante de la institución a través de las siguientes informaciones');
+
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+});
+
+
+$(document).ready(function(){
+	var zindex = 10;
+	
+	$("div.card").click(function(e){
+	  e.preventDefault();
+  
+	  var isShowing = false;
+  
+	  if ($(this).hasClass("show")) {
+		isShowing = true
+	  }
+  
+	  if ($("div.cards").hasClass("showing")) {
+		// a card is already in view
+		$("div.card.show")
+		  .removeClass("show");
+  
+		if (isShowing) {
+		  // this card was showing - reset the grid
+		  $("div.cards")
+			.removeClass("showing");
+		} else {
+		  // this card isn't showing - get in with it
+		  $(this)
+			.css({zIndex: zindex})
+			.addClass("show");
+  
+		}
+  
+		zindex++;
+  
+	  } else {
+		// no cards in view
+		$("div.cards")
+		  .addClass("showing");
+		$(this)
+		  .css({zIndex:zindex})
+		  .addClass("show");
+  
+		zindex++;
+	  }
+	  
+	});
+  });
+
+
+
+
+
+  
+
+
+  
+
+
+
